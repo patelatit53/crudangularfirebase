05ea7a59e9e2e06688065b43a961bf3f5d3be5da
Atit Patel <atitpatel@Atits-MacBook-Air.local>
2018-02-12 16:49:36 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCFMR7rOOYnSTbRDr5rUV4SsZcGh2l1nLg",
+    authDomain: "chatapp-70a28.firebaseapp.com",
+    databaseURL: "https://chatapp-70a28.firebaseio.com",
+    projectId: "chatapp-70a28",
+    storageBucket: "chatapp-70a28.appspot.com",
+    messagingSenderId: "308715837679"
+  }
+};
